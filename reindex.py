c5bef6b66e510c24170973956acd19fedb350fba
anthony-shaw <anthony.p.shaw@gmail.com>
2016-03-14 15:23:49 +1100

    Added validator class and a YAML schema

--- reindex.py
+++ reindex.py
@@ -3,6 +3,8 @@ import os
 import logging
 import sys
 
+from validation import validate_fittings
+
 LEVELS = {'debug': logging.DEBUG,
           'info': logging.INFO,
           'warning': logging.WARNING,
@@ -15,16 +17,7 @@ if len(sys.argv) > 1:
     logging.basicConfig(level=level)
 
 
-def raise_KeyError(msg=''): raise KeyError(msg)  #  Don't return anything.
 
-
-def check_existence_of_key(dictionary, key, path):
-    try:
-        dictionary[key] \
-            or raise_KeyError('%s not present' % key)
-    except KeyError:
-        logging.error("fittings %s missing required links",
-                      path)
 readme = ""
 
 with open("fittings/categories.yaml", "r") as categories_f:
@@ -41,31 +34,7 @@ with open("fittings/categories.yaml", "r") as categories_f:
 
             for file in files:
                 if file == "fittings.yaml":
+                    validate_fittings(os.path.join(subdir, file), readme, subdir)
 
-                    # check the yaml
-                    file_path = os.path.join(subdir, file)
-                    with open(file_path) as fitting_f:
-
-                        plan = fitting_f.read()
-                        documents = plan.split('\n---')
-                        for document in documents:
-                            if '\n' in document:
-                                settings = yaml.load(document)
-                                logging.info("Validated fittings %s",
-                                             file_path)
-
-                                # Check existence of docs
-                                check_existence_of_key(settings,
-                                                       'links', file_path)
-                                check_existence_of_key(settings,
-                                                       'information', file_path)
-                                readme += "\n### [%s](%s) \n\n<img src='%s' style='width: 64px;'/>\n%s" % (
-                                    settings['information'][0],
-                                    settings['links']['documentation'],
-                                    'https://raw.githubusercontent.com/DimensionDataCBUSydney/plumbery-contrib/master/%s/icon.png' % subdir.replace('\\','/'),
-                                    '\n'.join(settings['information'][0:])
-                                )
-                                # parameters may break remaining documents
-                                break
 with open("INDEX.md", "w") as readme_f:
     readme_f.write(readme)
